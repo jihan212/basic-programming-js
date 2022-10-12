65ae7516cab8556a0c64d58fe635c3df25ae7f6f
jihan212 <jihanbjashim@gmail.com>
2022-10-12 11:30:57 +0600

    program of odd-even & sum done

---
+++ Day-1/odd-even.js
@@ -0,0 +1,10 @@
+let numbers = [101, 102, 55, 12, 1101, 5, 3, 232];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 == 0) {
+        console.log(numbers[i]," is even");
+    } else {
+        console.log(numbers[i]," is odd");
+    }
+    
+}
---
+++ Day-1/sum.js
@@ -0,0 +1,9 @@
+let numbers = [101, 102, 55, 12, 1101];
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    let num = numbers[i];
+    sum += num;
+}
+console.log(sum);
