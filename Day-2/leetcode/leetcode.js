65a6c8cb129482e3f978047e52a9303b9cb1f996
jihan212 <jihanbjashim@gmail.com>
2022-10-18 00:13:09 +0600

    leetcode problem done

---
+++ Day-2/leetcode/leetcode.js
@@ -0,0 +1,11 @@
+let numbers = [2, 7, 11, 15];
+
+let target = 22;
+
+for (let i = 0; i < numbers.length; i++) {
+	for (let j = 0; j < numbers.length; j++) {
+		if (numbers[i] + numbers[j] == target) {
+			console.log([i, j]);
+		}
+	}
+}
