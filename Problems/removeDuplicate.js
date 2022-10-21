5b8061a007e74277e9e2e4b3ee7f01e979c2b0bb
jihan212 <jihanbjashim@gmail.com>
2022-10-21 09:37:55 +0600

    new removeDuplicate program

--- Problems/removeDuplicate.js
+++ Problems/removeDuplicate.js
@@ -1,13 +1,8 @@
 let numbers = [1, 2, 2, 3, 4, 4, 5, 5];
 
-let duplicate;
-let uniqueNumbers = [];
+console.log([...new Set(numbers)]);
 
-for (let i = 0; i < numbers.length; i++) {
-	if (numbers[i] == numbers[i - 1]) {
-		duplicate = duplicate + 1;
-	} else {
-		numbers[i - duplicate] = numbers[i];
-		console.log(numbers[i]);
-	}
-}
+// At first convert the array into set data structure by new Set()
+// then use spread operator(...)
+// console.log it
+/* Explanation: By this one line program it will return new array with no duplicate numbers cause first it converted into set data structure. and set doesn't keep duplicate elements. */
